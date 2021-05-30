a78e63cf09a74c1eae98b8c7e083d15c9d310d87
ych <hurrc04@gmail.com>
2021-05-30 18:11:26 +0900

    forward testing update

--- deepcrypto/optimization/__init__.py
+++ deepcrypto/optimization/__init__.py
@@ -2,8 +2,10 @@ from deepcrypto.data_utils.crawlers.binance_crawler import read_binance_data
 from deepcrypto.backtest import run_backtest_df, BacktestAccessor
 from sklearn.model_selection import ParameterGrid
 import ray, os, datetime, psutil
+import matplotlib.pyplot as plt
 import quantstats as qs
 import pandas as pd
+import numpy as np
 import warnings
 import tqdm
 import time
@@ -16,7 +18,11 @@ def generate_backtest_fn(metric_fn):
         df = df.copy()
         df = strategy(df, config)
         order_df, port_df = run_backtest_df(df, log_time=False)
-        return metric_fn(order_df, port_df), config, cnt
+        
+        try: metric = metric_fn(order_df, port_df)
+        except: metric = None
+        
+        return metric, config, cnt, order_df, port_df
     return backtest_fn
 
 
@@ -73,17 +79,19 @@ class OptimizerBase:
                     )
 
                 if len(self.process_queue) > self.n_cores:
-                    result, config, cnt = ray.get(self.process_queue[0])
+                    result, config, cnt, _, _ = ray.get(self.process_queue[0])
                     
-                    self.save_result(config, result, cnt)
+                    if not result is None:
+                        self.save_result(config, result, cnt)
+                        self.update(config, result)
 
-                    self.update(config, result)
                     self.process_queue = self.process_queue[1:]
 
                     if done:
                         for p in self.process_queue:
                             result, config, cnt = ray.get(p)
-                            self.save_result(config, result, cnt)
+                            if not result is None:
+                                self.save_result(config, result, cnt)
                             
             
             if done: break
@@ -128,8 +136,41 @@ class BruteForceOptimizer(OptimizerBase):
             return None, True
 
 
-class GeneticOptimizer(OptimizerBase):
+def do_forward_testing(data, optimizer_cls, n_chunks=4, best_metric_name="metric", mode_max=False, **kwargs):
+    data_chunk_len = len(data.index) // n_chunks
     
+    order_df_lst, port_df_lst = [], []
+    return_arr = None
+    initial_cash = 0
+
+    for i in range(1, n_chunks):
+        data_train = data[:(i)*data_chunk_len]
+        
+        if data_train.empty:
+            raise ValueError
+
+        data_test = data[(i)*data_chunk_len:np.clip((i+1)*data_chunk_len, 0, len(data.index))]
+
+        optim = optimizer_cls(data=data_train, **kwargs)
+        optim.optimize()
+        best_config = optim.get_best_result(best_metric_name, mode_max)
+
+        if not data_test.empty:
+            _, _, _, order_df, port_df = ray.get(
+                optim.backtest_fn.remote(data_test, best_config, optim.strategy, 0))
+            order_df_lst.append(order_df)
+            port_df_lst.append(port_df)
+
+    for port_df in port_df_lst:
+        if return_arr is None:
+            initial_cash = port_df["portfolio_value"].iloc[0]
+            return_arr = port_df["portfolio_value"] / initial_cash
+        return_arr = return_arr.append(port_df["portfolio_value"] / initial_cash * return_arr.iloc[-1])
+        return_arr = return_arr[~return_arr.index.duplicated()]
+        return_arr = return_arr.sort_index()
+
+    return return_arr, best_config, order_df_lst, port_df_lst
+
 
 def test():
 
@@ -152,23 +193,36 @@ def test():
         return df
 
     def metric_fn(order_df, port_df):
-        return {"sharpe" : qs.stats.sharpe(port_df["portfolio_value"].resample("1D").last())}
+        return {"metric" : qs.stats.sharpe(port_df["portfolio_value"].resample("1D").last())}
 
     config_dict = {
-        "ma1" : [5, 10, 20, 50, 200, 250, 500],
-        "ma2" : [5, 10, 20, 50, 200, 250, 500],
-        "time_cut" : [5, 10, 20, 50, 200, 250, 500],
-        "stop_loss" : [0.02, 0.05, 0.07, 0.1, 0.15, 0.2],
-        "take_profit" : [0.05, 0.07, 0.1, 0.15, 0.2, 0.5]
+        "ma1" : [5, 20, 50, 200, 500],
+        "ma2" : [5, 20, 50, 200, 500],
+        "time_cut" : [5, 20, 50, 200],
+        "stop_loss" : [0.02, 0.05, 0.1],
+        "take_profit" : [0.1, 0.2, 0.5]
     }
 
     data = read_binance_data("/home/ych/Storage/binance/binance.db", "1H", "BTCUSDT")
-    
-    optimizer = BruteForceOptimizer(data=data, strategy=crossover_strategy, config_dict=config_dict, metric_fn=metric_fn, result_dir="./deepcrypto_results", strategy_name="crossover_strategy", n_cores=12)
+    data = data.backtest.add_defaults()
 
-    optimizer.optimize()
-    
-    optimizer.save_results()
+    return_arr, best_config, order_df_lst, port_df_lst = do_forward_testing(
+        data=data, 
+        optimizer_cls=BruteForceOptimizer,
+        strategy=crossover_strategy, 
+        config_dict=config_dict, 
+        metric_fn=metric_fn, 
+        result_dir="./deepcrypto_results", 
+        strategy_name="crossover_strategy", 
+        n_cores=12
+    )
+    print(return_arr)
+    for i, port_df in enumerate(port_df_lst):
+        qs.reports.html(port_df["portfolio_value"], output=f"./samples/sample{i}.html")
+
+    return_arr.plot()
+
+    plt.savefig("./sample.png")
 
 
 if __name__ == "__main__":
