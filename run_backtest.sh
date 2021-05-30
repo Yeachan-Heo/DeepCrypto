5cb28b718845a7df7dbffd24ba646a6cb4f9dc9c
ych <hurrc04@gmail.com>
2021-05-30 18:13:39 +0900

    cleanup

--- run_backtest.sh
+++
@@ -1,9 +0,0 @@
-backtest \
-	--data_format sqlite3 \
-	--data_path /home/ych/Storage/binance/binance.db \
-	--table_name BTCUSDT_1H \
-	--columns tohlcv \
-	--strategy crossover.strategy \
-	--output_path sample_export \
-	--strategy_name crossover \
-	--config_path config.json 
