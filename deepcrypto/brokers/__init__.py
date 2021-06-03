251c965a7df651359b530e9f0757b362a79663de
ych <hurrc04@gmail.com>
2021-06-04 00:36:24 +0900

    implementing binance broker

--- deepcrypto/brokers/__init__.py
+++ deepcrypto/brokers/__init__.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import numpy as np
+import sqlite3
 
 from typing import *
 from deepcrypto.backtest import *
 
 
 class OrderTypes:
-    MARKET_BUY = 0
-    LIMIT_BUY = 1
+    MARKET = 0
 
-    MARKET_SELL = 2
-    LIMIT_SELL = 3
+    LIMIT = 0
     
     MARKET_SL = 4
     LIMIT_SL = 5
@@ -18,9 +17,12 @@ class OrderTypes:
     MARKET_TP = 6
     LIMIT_TP = 7
     
+class OrderSides:
+    BUY = 1
+    SELL = -1
 
 class BrokerBase():
-    def __init__(self, ticker, timeframe, strategy, config, n_bars, market_order=True):
+    def __init__(self, ticker, timeframe, strategy, config, n_bars, market_order=True, stoploss_market=True, takeprofit_market=False, log_db_path="./log.sqlite3"):
         self.ticker = ticker
         self.timeframe = timeframe
         self.strategy = strategy
@@ -30,7 +32,25 @@ class BrokerBase():
 
         self.data = self.init_data()
 
-        self.current_pos = 0
+        self.position_side = 0
+
+        flag = os.path.exists(log_db_path)
+        self.log_db = sqlite3.connect(log_db_path)
+
+        self.stoploss_market = stoploss_market
+        self.takeprofit_market = takeprofit_market
+
+        if not flag:
+            self.log_db.execute(
+                """CREATE TABLE LOG (
+                    timestamp int,
+                    portfolio_value float,
+                    price float,
+                    cash_left float,
+                    position_size float,
+                    position_side int
+                )""")
+            self.log_db.commit()
 
     def init_data(self) -> pd.DataFrame:
         raise NotImplementedError
@@ -41,13 +61,19 @@ class BrokerBase():
         """
         raise NotImplementedError
 
-    def order(self, quantity, type, price, **kwargs):
+    def order(self, quantity, type, side, price, **kwargs):
         raise NotImplementedError
 
     def cancel_all_orders(self):
         raise NotImplementedError
 
+    def update_account_info(self):
+        raise NotImplementedError
+
     def get_position_size(self):
+        raise NotImplementedError
+
+    def get_position_side(self):
         raise NotImplementedError
 
     def get_current_price(self):
@@ -57,44 +83,58 @@ class BrokerBase():
         raise NotImplementedError
 
     def get_portfolio_value(self):
+        self.update_account_info()
+
         cprice = self.get_current_price()
         cashleft = self.get_cash_left()
         position_size = self.get_position_size()
+        position_side = self.get_position_side()
 
-        return cprice * position_size + cashleft, cprice, cashleft, position_size
+        return cprice * position_size + cashleft, cprice, cashleft, position_size, position_side
         
 
     def trade_step(self):
         res = self.strategy(self.data, self.config).iloc[-1]
-        target_pos = order_logic(self.current_pos, res["enter_long"], res["enter_short"], res["close_long"], res["close_short"])
-        if target_pos != self.current_pos:
+        target_pos = order_logic(self.position_side, res["enter_long"], res["enter_short"], res["close_long"], res["close_short"])
+        
+        if target_pos != self.position_side:
             
-            target_amount = res["bet"] * self.portfolio_value / self.price - self.position_size * self.current_pos
+            target_amount = res["bet"] * self.portfolio_value / self.price - self.position_size * self.position_side
             order_size, order_side = np.abs(target_amount), np.sign(target_amount)
-
-            if order_side == -1:
-                order_type = OrderTypes.MARKET_SELL if self.market_order else OrderTypes.LIMIT_SELL
-            if order_side == 1:
-                order_type = OrderTypes.MARKET_BUY if self.market_order else OrderTypes.LIMIT_SELL
             
             self.cancel_all_orders()
             
-            self.order(order_size, order_type, self.price)
+            self.order(order_size, OrderTypes.LIMIT if not self.market_order else OrderTypes.MARKET, order_side, self.price)
 
             if not target_pos == 0:
                 if not res["stop_loss"] != np.inf:
-                    self.order(order_size, order_type, (self.price * (1 - res["stop_loss"] * target_pos)))
+                    order_type = OrderTypes.MARKET_SL if self.stoploss_market else OrderTypes.LIMIT_SL
+                    self.order(order_size, order_type, order_side, (self.price * (1 - res["stop_loss"] * target_pos)))
                 if not res["take_profit"] != np.inf:
-                    self.order(order_size, order_type, (self.price * (1 + res["take_profit"] * target_pos)))
+                    order_type = OrderTypes.MARKET_TP if self.stoploss_market else OrderTypes.LIMIT_TP
+                    self.order(order_size, order_type, order_side, (self.price * (1 + res["take_profit"] * target_pos)))
             
 
     def main(self):
-        _, closed = self.update_new_data_stream()
-        self.portfolio_value, self.price, self.cash_left, self.position_size = self.get_portfolio_value()
+        closed = self.update_new_data_stream()
+        self.portfolio_value, self.price, self.cash_left, self.position_size, self.position_side = self.get_portfolio_value()
+        
+        self.log_db.execute(
+            f"""
+            INSERT INTO LOG VALUES (
+                {self.portfolio_value},
+                {self.price},
+                {self.cash_left},
+                {self.position_size},
+                {self.position_side}
+            )
+            """
+        )
+        self.log_db.commit()
+
         if closed:
             self.trade_step()
         
-
 
 
     
--- deepcrypto/brokers/binance_broker.py
+++ deepcrypto/brokers/binance_broker.py
@@ -1,11 +1,76 @@
 import pandas as pd
 import numpy as np
-import websocket
-import json
-import pprint
-import talib
-import numpy
-import os
-import sqlite3
 from binance.client import Client
 from binance.enums import *
+from deepcrypto.brokers import *
+
+class BinanceBroker(BrokerBase):
+    ORDER_TYPE_DICT = {
+        OrderTypes.LIMIT_BUY : Client.ORDER_TYPE_LIMIT,
+        OrderTypes.LIMIT_ : Client.ORDER_TYPE_STOP_LOSS,
+    }
+
+    INTERVAL2MINUTE = {
+        Client.KLINE_INTERVAL_1MINUTE : 1,
+        Client.KLINE_INTERVAL_3MINUTE : 3,
+        Client.KLINE_INTERVAL_5MINUTE : 5,
+        Client.KLINE_INTERVAL_15MINUTE : 15,
+        Client.KLINE_INTERVAL_30MINUTE : 30,
+        Client.KLINE_INTERVAL_1HOUR : 60,
+        Client.KLINE_INTERVAL_2HOUR : 120,
+        Client.KLINE_INTERVAL_4HOUR : 240,
+        Client.KLINE_INTERVAL_6HOUR : 360,
+        Client.KLINE_INTERVAL_8HOUR : 480,
+        Client.KLINE_INTERVAL_12HOUR : 720,
+        Client.KLINE_INTERVAL_1DAY : 1440,
+        Client.KLINE_INTERVAL_3DAY : 1440 * 3,
+        Client.KLINE_INTERVAL_1WEEK : 1440 * 7,
+    }
+
+    def __init__(self, api_key, api_secret, testnet=False, **kwargs):
+        self.binance_client = Client(api_key, api_secret)
+        self.symbol = self.ticker.replace("/", "")
+        self.trade_asset, self.balance_asset = self.ticker.split("/")
+        super(BinanceBroker, self).__init__(**kwargs)
+        
+    
+    def init_data(self) -> pd.DataFrame:
+        data = self.binance_client.get_historical_klines(f"{self.n_bars * self.INTERVAL2MINUTE[self.timeframe]} minutes ago UTC")
+        data = np.array(data[:-1], dtype=np.float32)[:, :6]
+        data = pd.DataFrame(data, columns=["time", "open", "high", "low", "close", "volume"])
+        data.index = pd.to_datetime(data["time"]*1000000)
+        return data
+
+    def update_new_data_stream(self) -> Tuple[pd.DataFrame, bool]:
+        data = self.binance_client.get_klines(self.symbol, self.timeframe, limit=2)
+        closed=False
+        if data[0][0] != self.data["time"].iloc[-1]:
+            closed = True
+            new_data_stream = pd.DataFrame(
+                [data[0]], 
+                columns=["time", "open", "high", "low", "close", "volume"], 
+                index=[pd.to_datetime(data[0][0] * 1000000)]
+            )
+            self.data = self.data.iloc[1:].append(new_data_stream)
+        return closed
+
+    def order(self, quantity, type, price, **kwargs):
+        raise NotImplementedError
+
+    def cancel_all_orders(self):
+        raise NotImplementedError
+
+    def update_account_info(self):
+        raise NotImplementedError
+
+    def get_position_size(self):
+        raise NotImplementedError
+
+    def get_position_side(self):
+        raise NotImplementedError
+
+    def get_current_price(self):
+        raise NotImplementedError
+
+    def get_cash_left(self):
+        raise NotImplementedError
